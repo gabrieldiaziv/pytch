b982f3ab9ce63e6d5312b4eca3dffafc166db2d4
rynoson <sonlimryan@gmail.com>
2023-10-23 17:59:39 -0500

    adding test cards

--- frontend/src/app/upload/page.tsx
+++ frontend/src/app/upload/page.tsx
@@ -2,6 +2,14 @@
 
 import {useDropzone} from 'react-dropzone'
 import { useCallback } from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function MyDropzone() {
   const onDrop = useCallback(acceptedFiles => {
@@ -10,7 +18,10 @@ function MyDropzone() {
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
-    <div className="text-center p-16 mt-10 bg-gray-100 border rounded-md border-neutral-200" {...getRootProps()}>
+    <div className="text-black text-center p-16 bg-gray-100 border rounded-md border-neutral-200" {...getRootProps()}>
+
+    {/* For menu bar later ?? */}
+    {/* <div className="self-end text-center w-4/5 p-16 mt-10 bg-gray-100 border rounded-md border-neutral-200" {...getRootProps()}>  */}
 
     <div className="flex justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="purple" className=" w-12 h-12">
@@ -19,8 +30,6 @@ function MyDropzone() {
 
     </div>
     
-
-
       <input {...getInputProps()} />
       <p className='text-[#800080]'>Dropzone</p>
       {
@@ -33,20 +42,30 @@ function MyDropzone() {
 }
 
 export default async function UploadPage() {
-
+    let items = ["test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test"];
+    let itemList=[];
+    items.forEach((item,index)=>{
+        itemList.push( 
+            <Card style={{backgroundImage: `url(${'./assets/newjeans.png'})`}} className="flex flex-none h-4/5 w-1/5 bg-gray-100">
+                <CardContent className="p-2 self-end text-white">
+                    {item}
+                </CardContent>
+            </Card>
+        )
+    })
   return (
     <div className="flex flex-col w-full h-[100svh]">
       <div className="flex w-full h-[10%]">
 
       </div>
       <div className="flex w-full h-[90%]">
-        <div className="flex flex-col w-full h-1/2 p-4 gap-6">
+        <div className="flex flex-col h-full w-full p-4 gap-6">
             <MyDropzone/>
-          <h1 className="text-3xl font-semibold">More to come yuhh</h1>
-            
-          <div>
-          </div>
-          <div className="flex h-full">
+
+          
+    
+          <div className="flex flex-wrap h-full gap-4">
+                {itemList}
           </div>
         </div>
       </div>
